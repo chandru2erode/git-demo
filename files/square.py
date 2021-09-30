3652fddf33ef4a3f9731ee6a7b98ff64cebfb327
Talentsprint <chandrakumar.k@talentsprint.com>
2021-09-30 14:38:20 +0530

    Changes in few files

--- files/square.py
+++ files/square.py
@@ -4,5 +4,6 @@ def cube(num):
 if __name__ == "__main__":
     numbers = [1, 2, 3, 4, 5]
     for each in numbers:
-        print(cube(each)
+        print(f"Cube of {each}: {cube(each)}")
+
     print('Calculation completed.')
